3a0f470a9ab8cd82db46a93b10532fc5a9c03747
Raphael Scursoni <ra.scursoni@gmail.com>
2021-02-21 15:10:27 -0300

    Aprendendo a usar lambda em LINQ testes 1

---
+++ LINQ-Lambda/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_Lambda.Entities {
+    class Category {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
